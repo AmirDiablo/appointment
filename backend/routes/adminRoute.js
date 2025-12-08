010cb11bd684aef0557955772c74cf4ab100ba2c
Amirdiablo <mmdlivi2022@gmail.com>
2025-12-08 12:43:49 +0330

    add admin login api

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { addDoctors } from "../controllers/adminController.js"
+import { addDoctors , adminLogin} from "../controllers/adminController.js"
 import upload from "../middlewares/multer.js"
 
 const router = express.Router()
 
 router.post("/add-doctor", upload.single("image"), addDoctors)
+router.post("/login", adminLogin)
 
 export default router
