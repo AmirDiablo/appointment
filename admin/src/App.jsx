4d33e8b42d391e895307a6532046ce269efd1fb2
Amirdiablo <mmdlivi2022@gmail.com>
2025-12-09 17:20:36 +0330

    create login for admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,11 +1,16 @@
 import {Routes, Route} from "react-router-dom"
+import Login from "./pages/Login"
+import {ToastContainer, toast} from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 
 function App() {
 
   return (
-    <>
+    <div>
+      <Login />
+      <ToastContainer />
+    </div>
       
-    </>
   )
 }
 
