ec77f9c445114af38c49363faf3156253f061d61
Amirdiablo <mmdlivi2022@gmail.com>
2025-12-12 10:14:32 +0330

    create api for changing doctors availability

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -1,5 +1,16 @@
+import Doctor from "../models/doctorModel.js"
 
 
+const changeAvailibility = async (req, res) => {
+    try {
+        const {docId} = req.body
+        
+        const docData = await Doctor.findById(docId)
+        await Doctor.findByIdAndUpdate(docId, {available: !docData.available})
+        res.json({success: true, message: "Availability changed"})
+    } catch (error) {
+        res.json({success: false, message: error.message})
+    }
+}
 
-
-
+export {changeAvailibility}
--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -2,11 +2,13 @@ import express from "express"
 import { addDoctors , adminLogin, allDoctors} from "../controllers/adminController.js"
 import upload from "../middlewares/multer.js"
 import authAdmin from "../middlewares/AuthAdmin.js"
+import { changeAvailibility } from "../controllers/doctorController.js"
 
 const router = express.Router()
 
 router.post("/add-doctor", authAdmin, upload.single("image"), addDoctors)
 router.post("/login", adminLogin)
 router.post("/all-doctors", authAdmin,allDoctors)
+router.post("/change-availability", authAdmin, changeAvailibility)
 
 export default router
